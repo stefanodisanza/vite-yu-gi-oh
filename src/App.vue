ef187abf9fcd59d7dff2a9b3512a8cc70f5eae4a
stefanodisanza <disanzastefano@gmail.com>
2023-03-03 15:39:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    YU-GI-OH
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
