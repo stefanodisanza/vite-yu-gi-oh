cc49f79757d1196027b8cb9067fb554179661e1e
stefanodisanza <disanzastefano@gmail.com>
2023-03-03 17:12:25 +0100

    FEAT main done

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,10 @@
 <template>
     <main class="main">
         <ul class="container">
-            <li v-for="character in characters" :key="charachter.id">
-                nome personaggio
+            <li v-for="character in characters" :key="character.id" class="card">
+                <img :src="character.card_images[0].image_url" alt="">
+                <h3>{{ character.name }}</h3>
+                <h5>{{ character.archetype }}</h5>
             </li>
         </ul>
     </main>
@@ -40,5 +42,28 @@ export default {
 .main {
     padding: 100px 0;
     background-color: chocolate;
+}
+
+img {
+    width: 300px;
+    padding: 25px;
+}
+
+.container {
+    display: flex;
+    gap: 25px;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+}
+
+.card {
+    border: 4px solid black;
+    background-color: darkolivegreen;
+}
+
+h5 {
+    margin-bottom: 20px;
 }
 </style>
